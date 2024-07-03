5e907fcc023e9cf82bed1a90127c721d2d45b6f8
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-03 09:07:10 -0400

    Lasagna: complete

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -33,7 +33,12 @@
 /**
  * The number of minutes it takes to prepare a single layer.
  */
-const PREPARATION_MINUTES_PER_LAYER = 2;
+const PREPARATION_MINUTES_PER_LAYER = 2
+
+/**
+ * The expected cook time in minutes.
+ */
+export const EXPECTED_MINUTES_IN_OVEN = 40
 
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
@@ -43,7 +48,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
 }
 
 /**
@@ -53,7 +58,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  throw new Error('Remove this line and implement the function');
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER
 }
 
 /**
@@ -65,5 +70,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
 }

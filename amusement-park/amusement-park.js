06d8655c8cef32034bf1136654a4b9193bdddf34
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-04 18:58:52 -0400

    Amusement Park: first iteration

--- amusement-park/amusement-park.js
+++ amusement-park/amusement-park.js
@@ -10,7 +10,7 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  return { name, age, ticketId }
 }
 
 /**
@@ -20,7 +20,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+  visitor.ticketId = null
+  return visitor
 }
 
 /**
@@ -31,7 +32,12 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+  const status = tickets[ticketId]
+  return status === undefined
+    ? 'unknown ticket id'
+    : status === null
+      ? 'not sold'
+      : 'sold to ' + status
 }
 
 /**
@@ -43,7 +49,7 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
+  return tickets[ticketId] ?? 'invalid ticket !!!'
 }
 
 /**
@@ -53,5 +59,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  return visitor?.gtc?.version
 }

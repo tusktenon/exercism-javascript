d87795c2d74fb630074a885e718e147642ad1e66
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-04 22:59:29 -0400

    Lasagna Master: first iteration

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,38 @@
  *   ...
  * }
  */
+
+export function cookingStatus(minutes) {
+  switch (minutes) {
+    case undefined:
+    case null:
+      return 'You forgot to set the timer.'
+    case 0:
+      return 'Lasagna is done.'
+    default:
+      return 'Not done, please wait.'
+  }
+}
+
+export function preparationTime(layers, averageTime = 2) {
+  return layers.length * averageTime
+}
+
+export function quantities(layers) {
+  const noodlesLayers = layers.filter((layer) => layer === 'noodles').length
+  const sauceLayers = layers.filter((layer) => layer === 'sauce').length
+  return { noodles: 50 * noodlesLayers, sauce: 0.2 * sauceLayers }
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  const secretIngredient = friendsList[friendsList.length - 1]
+  myList.push(secretIngredient)
+}
+
+export function scaleRecipe(recipe, portions) {
+  const scaled = {}
+  for (const key in recipe) {
+    scaled[key] = (portions / 2) * recipe[key]
+  }
+  return scaled
+}

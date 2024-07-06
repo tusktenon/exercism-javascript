b4433c464829485b378b608c27e964a828cf9d0d
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-06 12:25:08 -0400

    Elyses Looping Enchantments: first iteration

--- elyses-looping-enchantments/enchantments.js
+++ elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,11 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0
+  stack.forEach((c) => {
+    if (c === card) count++
+  })
+  return count
 }
 
 /**
@@ -20,5 +24,10 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+  let count = 0
+  for (const card of stack) {
+    let even = card % 2 === 0
+    if ((type && even) || (!type && !even)) count++
+  }
+  return count
 }

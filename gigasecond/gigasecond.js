e6712912186a835039472773663ad8b803a90780
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-13 16:42:03 -0400

    Gigasecond: complete

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+'use strict'
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const gigasecond = date => new Date(date.getTime() + 1e12)

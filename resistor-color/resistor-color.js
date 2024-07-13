a216b44f7cd388e91c44a7000b7ba130c5e41ee5
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-13 13:05:04 -0400

    Resistor Color: complete

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,10 +1,14 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
-};
-
-export const COLORS = undefined;
+export const colorCode = color => COLORS.indexOf(color)

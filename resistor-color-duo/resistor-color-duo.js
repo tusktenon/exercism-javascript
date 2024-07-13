53d2c9ff4d94e0c3466b7c643be7656108152dfd
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-13 16:17:24 -0400

    Resistor Color Duo: add alternate solution

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -13,3 +13,15 @@ const COLORS = [
 
 export const decodedValue = ([first, second]) =>
   10 * COLORS.indexOf(first) + COLORS.indexOf(second)
+
+// An interesting community solution by slaymance:
+// https://exercism.org/tracks/javascript/exercises/resistor-color-duo/solutions/slaymance
+// Overkill for this particularly problem, it would be an elegant approach if
+// more than two colors were involved.
+export const decodedValueAlt = colors =>
+  Number(
+    colors
+      .slice(0, 2)
+      .map(color => COLORS.indexOf(color))
+      .join(''),
+  )

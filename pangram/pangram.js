2391dfffebd0b5b3b3a57ab0542f03a273bafe44
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-15 18:46:59 -0400

    Pangram: first iteration

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,12 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+'use strict'
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const isPangram = sentence => {
+  const found = Array.from({ length: 26 }, () => false)
+
+  ;[...sentence.toLowerCase()]
+    .map(c => c.codePointAt(0) - 'a'.codePointAt(0))
+    .filter(x => 0 <= x && x < 26)
+    .forEach(x => (found[x] = true))
+
+  return found.every(x => x)
+}

fc849042c816a061ffe4d604982e0bfc043ee523
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-19 21:39:53 -0700

    Matrix: second iteration

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,20 +1,6 @@
 export class Matrix {
   constructor(string) {
-    this.rows = string
-      .split('\n')
-      .map(rowString =>
-        rowString.split(' ').map(elementString => Number(elementString)),
-      )
-
-    this.columns = []
-    if (this.rows.length !== 0) {
-      for (let j = 0; j < this.rows[0].length; j++) {
-        const col = []
-        for (let i = 0; i < this.rows.length; i++) {
-          col.push(this.rows[i][j])
-        }
-        this.columns.push(col)
-      }
-    }
+    this.rows = string.split('\n').map(row => row.split(' ').map(Number))
+    this.columns = this.rows[0].map((_, i) => this.rows.map(row => row[i]))
   }
 }

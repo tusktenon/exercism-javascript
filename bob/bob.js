7163dd5a7320049533ce0105dc043421a1e1f14a
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-20 15:44:25 -0700

    Bob: add alternative solution

--- bob/bob.js
+++ bob/bob.js
@@ -1,8 +1,16 @@
 export function hey(message) {
+  // Select an implementation:
+  return hey2(message)
+}
+
+// Option 1: Use chained conditional operators
+function hey1(message) {
   const trimmed = message.trim()
   if (trimmed === '') return 'Fine. Be that way!'
+
   const question = trimmed.endsWith('?')
   const yelling = /\p{Lu}/u.test(trimmed) && !/\p{Ll}/u.test(trimmed)
+
   return question && yelling
     ? "Calm down, I know what I'm doing!"
     : question
@@ -10,4 +18,24 @@ export function hey(message) {
       : yelling
         ? 'Whoa, chill out!'
         : 'Whatever.'
+}
+
+// Option 2: Use a switch statement
+function hey2(message) {
+  const trimmed = message.trim()
+  if (trimmed === '') return 'Fine. Be that way!'
+
+  const question = trimmed.endsWith('?')
+  const yelling = /\p{Lu}/u.test(trimmed) && !/\p{Ll}/u.test(trimmed)
+
+  switch (true) {
+    case question && yelling:
+      return "Calm down, I know what I'm doing!"
+    case question:
+      return 'Sure.'
+    case yelling:
+      return 'Whoa, chill out!'
+    default:
+      return 'Whatever.'
+  }
 }

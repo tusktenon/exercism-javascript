425ad1f15fa69afb516739fec0d02b9591c6d93f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-20 15:29:51 -0700

    Bob: tweak two conditions

--- bob/bob.js
+++ bob/bob.js
@@ -1,8 +1,8 @@
 export function hey(message) {
   const trimmed = message.trim()
-  if (trimmed.length === 0) return 'Fine. Be that way!'
+  if (trimmed === '') return 'Fine. Be that way!'
   const question = trimmed.endsWith('?')
-  const yelling = /\p{L}/u.test(trimmed) && !/\p{Ll}/u.test(trimmed)
+  const yelling = /\p{Lu}/u.test(trimmed) && !/\p{Ll}/u.test(trimmed)
   return question && yelling
     ? "Calm down, I know what I'm doing!"
     : question

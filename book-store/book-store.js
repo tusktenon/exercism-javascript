abe5fd2e0592121fa174a48a5cc60863c85a034f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-22 22:11:26 -0700

    Book Store: first iteration

--- book-store/book-store.js
+++ book-store/book-store.js
@@ -1,8 +1,36 @@
-//
-// This is only a SKELETON file for the 'BookStore' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const PRICE_PER_BOOK = [0, 800, 760, 720, 640, 600]
 
-export const cost = (books) => {
-  throw new Error('Remove this statement and implement this function');
-};
+export function cost(books) {
+  return books.length === 0 ? 0 : costHelper(getCounts(books))
+}
+
+function costHelper(counts) {
+  if (counts[0] === counts[counts.length - 1])
+    return counts.reduce((sum, c) => sum + c) * PRICE_PER_BOOK[counts.length]
+
+  let price = Infinity
+  for (let s = 1; s < counts.length; s++) {
+    price = Math.min(
+      price,
+      s * PRICE_PER_BOOK[s] + costHelper(removeSet(counts, s)),
+    )
+  }
+  return price
+}
+
+function getCounts(books) {
+  return books
+    .reduce((counts, book) => {
+      counts[book] = (counts[book] ?? 0) + 1
+      return counts
+    }, [])
+    .filter(x => x)
+    .sort((a, b) => b - a)
+}
+
+function removeSet(counts, setSize) {
+  return counts
+    .map((x, i) => (i < setSize ? x - 1 : x))
+    .filter(x => x > 0)
+    .sort((a, b) => b - a)
+}

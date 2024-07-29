548f1d5a080640fdf53d91d420fae925626b336a
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-07-29 09:27:40 -0700

    Pascal's Triangle: first iteration

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,8 +1,13 @@
-//
-// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const rows = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export function rows(n) {
+  let triangle = []
+  for (let i = 0; i < n; i++) {
+    const row = []
+    row[0] = 1
+    row[i] = 1
+    for (let j = 1; j < i; j++) {
+      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    }
+    triangle.push(row)
+  }
+  return triangle
+}

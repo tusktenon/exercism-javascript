269732fef1472c3dc84b6ef738c56faa774e111d
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-18 10:41:57 -0500

    Two Fer: use offical name for ?? operator

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,5 +1,5 @@
 // Option 1: Use a default argument
 // export const twoFer = (name = 'you') => `One for ${name}, one for me.`
 
-// Option 2: Use the first-defined operator
+// Option 2: Use the nullish-coalescing operator
 export const twoFer = name => `One for ${name ?? 'you'}, one for me.`
